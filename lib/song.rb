09ad02bdc06cf21e6743414600a0017000ae4acf
Cristina Lugo <cmlugoce@syr.edu>
2018-06-18 16:08:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,11 @@ class Song
     artist.add_song(self)
   end
 
-  def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
+  def self.new_from_filename(filename)
+    filename = filename.gsub(".mp3","")
+    artist_name, song_name, genre_name = filename.split(" - ")
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
   end
 end
